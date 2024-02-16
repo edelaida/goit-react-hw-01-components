7f32395c86c95f04ccc9a08951a4fd2e1761b434
Levina Tani <edelaida@gmail.com>
2024-02-16 09:45:47 +0200

    new Profile

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -5,10 +5,19 @@ import { FriendList } from './FriendList/FriendList.jsx';
 import friendItem from '../assets/friends.json';
 import { TransactionHistory } from './TransactionHistory/TransactionHistory.jsx';
 import historiElem from '../assets/transactions.json';
+import { Profile } from './Profile/Profile.jsx';
+import user from '../assets/user.json';
 
 function App() {
   return (
     <div>
+      <Profile
+        username={user.username}
+        tag={user.tag}
+        location={user.location}
+        avatar={user.avatar}
+        stats={user.stats}
+      />
       <Statistics title="Upload stats" data={dataStats} />
       <FriendList friends={friendItem} />
       <TransactionHistory transactions={historiElem} />
