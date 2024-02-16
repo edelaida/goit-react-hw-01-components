ba9cd63541f94f0dd14b8269093e941a87693d8b
Levina Tani <edelaida@gmail.com>
2024-02-16 08:08:08 +0200

    changes profile

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,12 +1,13 @@
 import { Profile } from './Profile/Profile.jsx';
 import '../index.css';
-import user from '../assets/user.json';
 import { Statistics } from './Statistics/Statistics.jsx';
 import dataStats from '../assets/data.json';
 import { FriendList } from './FriendList/FriendList.jsx';
 import friendItem from '../assets/friends.json';
 import { TransactionHistory } from './TransactionHistory/TransactionHistory.jsx';
 import historiElem from '../assets/transactions.json';
+
+import user from '../assets/user.json';
 
 function App() {
   return (
